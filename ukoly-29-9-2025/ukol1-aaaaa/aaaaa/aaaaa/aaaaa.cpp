75d6acb4f444d301fdc6376ab08b2718e79964b9
Tvé jméno <adam.bucnak@gmail.com>
2025-09-29 12:39:29 +0200

    noveukoly

---
+++ ukoly-29-9-2025/ukol1-aaaaa/aaaaa/aaaaa/aaaaa.cpp
@@ -0,0 +1,20 @@
+﻿// aaaaa.cpp : Tento soubor obsahuje funkci main. Provádění programu se tam zahajuje a ukončuje.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main() {
+    char arr[] = { 'a', 'b', 'c', 'd', 'e' };
+    
+
+    srand(time(NULL)); 
+
+    int nahodnyIndex = rand() % 4;
+
+    printf("Náhodný index: %d\n", nahodnyIndex);
+    printf("Znak na tomto indexu: %c\n", arr[nahodnyIndex]);
+
+    return 0;
+}
